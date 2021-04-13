7d7e36352c88aaca2a610909b4a135089d4315b7
Jiayu Zhou <dearjiayu@gmail.com>
2021-04-13 10:24:48 -0400

    Create download.sh

---
+++ data/download.sh
@@ -0,0 +1,11 @@
+# Script for downloading the datasets 
+# to this folder. 
+
+# TODO
+# 1. Upload the data to a static source 
+#    Ex. Please research on https://git-lfs.github.com/ and see the pricing
+#    Alternatively we can use a Dropbox link / GCP Bucket/ Amazon S3. 
+#    Q: which one you think is better? 
+# 2. Use `wget` to download the data files to this folder from the links above.
+#    Additionally, add the files to the gitignore, so this repo does not track. 
+# 3. Adjust the configuration in the `source` folder. 
